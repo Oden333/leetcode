d7097f16df78f5e3dff5f00df3918fe94f690915
Ismail <galimov-ismail@mail.ru>
2026-02-21 16:04:11 +0300

    update keybindings.json, settings.json, main.go and 76.go

--- main.go
+++ main.go
@@ -5,16 +5,10 @@ import (
 	"os"
 )
 
-// "fmt"
-// . "leetcode/Structs"
-// . "leetcode/medium"
-
 func main() {
 	fmt.Println("GOTESTS_TEMPLATE_DIR =", os.Getenv("GOTESTS_TEMPLATE_DIR"))
 
-	// Посмотреть все переменные окружения
 	for _, e := range os.Environ() {
-		// если хотите увидеть все переменные, раскомментируйте:
 		fmt.Println(e)
 	}
 }
