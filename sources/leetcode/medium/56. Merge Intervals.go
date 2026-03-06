e2ab5f536e068b407db8c334a45b9a4424293585
Ismail <galimov-ismail@mail.ru>
2026-03-06 11:56:44 +0300

    update settings.json, '56. Merge Intervals.go' and '56. Merge Intervals_test.go'

--- sources/leetcode/medium/56. Merge Intervals.go
+++ sources/leetcode/medium/56. Merge Intervals.go
@@ -1,6 +1,47 @@
 package medium
 
-func merge(intervals [][]int) [][]int {
+import "slices"
 
-	return intervals
+func merge(intervals [][]int) [][]int {
+	if len(intervals) <= 1 {
+		return intervals
+	}
+	slices.SortFunc(intervals, func(a, b []int) int {
+		switch {
+		case a[0] < b[0]:
+			return -1
+		case a[0] == b[0]:
+			switch {
+			case a[1] < b[1]:
+				return -1
+			case a[1] == b[1]:
+				return 0
+			case a[1] > b[1]:
+				return 1
+			}
+		}
+		return 1
+	})
+
+	var (
+		res  = make([][]int, 0)
+		last = intervals[0]
+		j    = 1
+	)
+
+	for j < len(intervals) {
+		b := intervals[j]
+		if last[1] >= b[0] {
+			if last[1] < b[1] {
+				last[1] = b[1]
+			}
+		} else {
+			res = append(res, last)
+			last = b
+		}
+		j++
+	}
+	res = append(res, last)
+
+	return res
 }
--- sources/leetcode/medium/56. Merge Intervals_test.go
+++ sources/leetcode/medium/56. Merge Intervals_test.go
@@ -1,1 +1,40 @@
 package medium
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_merge(t *testing.T) {
+	type args struct {
+		intervals [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			args: args{[][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}},
+			want: [][]int{{1, 6}, {8, 10}, {15, 18}},
+		},
+		{
+			args: args{[][]int{{1, 4}, {4, 5}}},
+			want: [][]int{{1, 5}},
+		},
+		{
+			args: args{[][]int{{4, 7}, {1, 4}}},
+			want: [][]int{{1, 7}},
+		},
+		{
+			args: args{[][]int{{1, 4}, {2, 3}}},
+			want: [][]int{{1, 4}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, merge(tt.args.intervals), "merge(%v)", tt.args.intervals)
+		})
+	}
+}
